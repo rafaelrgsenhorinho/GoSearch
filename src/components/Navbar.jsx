cf99daf4382641bf5fb4883f5602137567534111
Rafael <rafaelrgsenhorinho@gmail.com>
2024-10-21 11:22:58 -0300

    fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,15 +16,9 @@ export const Navbar = ({ darkTheme, setDarkTheme }) => {
 
     return (
         <div className="p-5 pb-0 h-50 flex flex-wrap justify-start items-start border-b dark:border-gray-700 border-gray-200">
+
             <div className="flex flex-wrap justify-center items-start space-x-5 w-full">
-                <h1
-                    className="text-2xl bg-blue-500 font-bold text-white py-2 px-4 rounded-2xl dark:bg-transparent dark:border-solid dark:border dark:border-white dark:text-white cursor-pointer"
-                    onClick={resetGoSearch}
-                >
-                    GoSearch
-                </h1>
-                <Search resetGoSearch={resetGoSearch} resetSearch={resetSearch} setResetSearch={setResetSearch} />
-                <label className="inline-flex items-center cursor-pointer m-3">
+                <label className="inline-flex items-center cursor-pointer mt-1 -rotate-90">
                     <input
                         type="checkbox"
                         className="sr-only peer"
@@ -32,8 +26,16 @@ export const Navbar = ({ darkTheme, setDarkTheme }) => {
                         checked={darkTheme}
                         readOnly
                     />
-                    <div className="relative w-14 h-7 bg-gray-200 rounded-full peer hover:ring-2 hover:ring-blue-400 dark:hover:ring-gray-200 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-['☀️'] peer-checked:after:content-['🌑'] after:absolute after:left-[4px] after:h-6 after:w-6 after:flex after:items-center after:justify-center after:text-xl after:rounded-full after:transition-all dark:border-gray-600 peer-checked:bg-gray-300 "></div>
+                    <div className="relative w-14 h-7 mt-6 bg-gray-200 rounded-full peer hover:ring-2 hover:ring-blue-400 dark:hover:ring-gray-200 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-['☀️'] peer-checked:after:content-['🌑'] after:absolute after:left-[4px] after:h-6 after:w-6 after:flex after:items-center after:justify-center after:text-xl after:rounded-full after:transition-all dark:border-gray-600 peer-checked:bg-gray-300 "></div>
                 </label>
+                <h1
+                    className="justify-items-start text-2xl bg-blue-500 font-bold text-white py-2 px-4 mt-2 mb-10 sm:mb-0 rounded-2xl dark:bg-transparent dark:border-solid dark:border dark:border-white dark:text-white cursor-pointer"
+                    onClick={resetGoSearch}
+                >
+                    GoSearch
+                </h1>
+                <Search resetGoSearch={resetGoSearch} resetSearch={resetSearch} setResetSearch={setResetSearch} />
+                
             </div>
         </div>
     );
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -22,17 +22,18 @@ export const Search = ( { resetSearch, setResetSearch } ) => {
 
 
   return (
-    <div className='relative sm:ml-48 md:ml-72 '>
+    <div className='relative sm:ml-48 md:ml-72 justify-start items-start'>
       <input
           value={text}
           type='text'
-          className='sm:w-96 w-80 h-10 dark:bg-gray-200 border rounded-full shadow-sm outline-none p-6 text-black hover:shadow-lg'
+          className='w-[15rem] md:w-[24rem] sm:w-[22rem] my-2 h-10 dark:bg-gray-200 border rounded-full shadow-sm outline-none p-6 text-black hover:shadow-lg'
           placeholder='Type and search'
           onChange={(e) => setText(e.target.value)}
       />
-        <button type='button' className='relative top-1.2 -right-2 text-2x text-gray-500 rounded-3xl bg-white p-2.5 px-5 drop-shadow-md hover:scale-105 duration-200 active:bg-gray-300' onClick={handleSearch}>
+        <button type='button' className='relative top-1.2 sm:-right-2 text-2x text-gray-500 rounded-3xl bg-white p-2.5 px-5 drop-shadow-md hover:scale-105 duration-200 active:bg-gray-300' onClick={handleSearch}>
           Search!
         </button>
+        
       <Links />
     </div>
   );
